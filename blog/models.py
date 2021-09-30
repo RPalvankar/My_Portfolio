37d52de7a13b115af3e83134bf575682b2b902df
Ritvik Palvankar <rpalvankar@ufl.edu>
2021-09-30 02:26:28 -0400

    Added Blogs

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Blog(models.Model):
     title=models.CharField(max_length=200)
-    description=models.TextField()
+    description=models.TextField(max_length=40000)
     date=models.DateField()
 
     def __str__(self):
